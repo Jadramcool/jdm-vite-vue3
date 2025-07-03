7a8f9088f6139cdaa87b3f9ab411c21fd8033052
jdm <1051780106@qq.com>
2025-07-03 11:07:52 +0800

    refactor(blog): 移除首页的个人信息展示区域及样式
    简化首页结构，移除不再需要的个人信息展示区域和相关样式代码

--- src/views/blog/home/index.vue
+++ src/views/blog/home/index.vue
@@ -1,18 +1,6 @@
 <template>
   <div>
     <Wave></Wave>
-    <div class="content">
-      <div class="left-side">
-        <div class="my-info w-200px h-200px flex">
-          <div class="avatar"></div>
-          <div class="name text-30px font-bold">Jay</div>
-          <div class="info">
-            <div class="desc">前端开发，关注技术，分享知识</div>
-          </div>
-        </div>
-      </div>
-      <div class="right-side"></div>
-    </div>
   </div>
 </template>
 
@@ -20,27 +8,4 @@
 import { Wave } from './components';
 </script>
 
-<style lang="scss" scoped>
-.content {
-  display: flex;
-  flex-direction: row;
-  height: 100%;
-  padding: 20px 10%;
-  .left-side {
-    flex: 1;
-    height: 100%;
-    .my-info {
-      background: linear-gradient(135deg, #ff6b6b, #ffb86c);
-      border-radius: 20px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    }
-  }
-  .right-side {
-    flex: 1;
-    height: 100%;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
